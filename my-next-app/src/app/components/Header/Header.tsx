799af5b73b0c41d070c4d1cc7fb560fab266040c
jaehyeong305 <jaehyeong.ha.305@gmail.com>
2023-08-29 16:28:20 +0900

    FICOS-2 add hover event: in icon component

--- my-next-app/src/app/components/Header/Header.tsx
+++ my-next-app/src/app/components/Header/Header.tsx
@@ -5,13 +5,20 @@ import Icon from "../Icon/Icon";
 const Header: React.FC = () => {
     const iconStyle: CSSProperties = {
         width: '40px',
+        height: '40px',
+        margin: '5px',
+    }
+
+    const wrapperStyle: CSSProperties = {
+        width: '50px',
+        height: '50px',
         marginRight: '20px',
     }
 
     return (
         <div className={styles.headerWraaper}>
             <div className={styles.headerLogo}>FICOS</div>
-            <Icon icon="menu" style={iconStyle}/>
+            <Icon icon="menu" iconStyle={iconStyle} wrapperStyle={wrapperStyle} canHover={true}/>
         </div>
     )
 }
